8d1918e8119a0b76c51084e39a9ee03a41483985
Naveen-3354 <welbe1709@gmail.com>
2024-02-23 18:48:43 +0530

    sidebar update in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,59 @@
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div id="navbar">
-        <button>click</button>
-    </div>
-  )
-}
+  const [style, setStyle] = useState({});
+  const [button, setButton] = useState(true);
 
-export default Navbar
+  const handleClick = () => {
+    console.log("click");
+
+    setButton(!button);
+
+    button
+      ? setStyle({
+          ...style,
+          left: "0",
+          borderRadius: "0%",
+          height: "100vh",
+          top: "0%",
+          width: "300px",
+        })
+      : setStyle({});
+  };
+  return (
+    <div id="navbar" className="relative">
+      <div
+        style={{}}
+        className="w-12 h-12 rounded-full text-center cursor-pointer absolute top-4 left-4 bg-orange-500 "
+        onClick={handleClick}
+      >
+        <Button click={button}/>
+      </div>
+      <div className="block absolute w-screen h-screen backdrop-brightness-70 top-0 -z-10">
+        <div
+          className="sidebar h-screen bg-slate-500 absolute"
+          style={{
+            width: "50vh",
+            height:"50vh",
+            top:"25%",
+            left:"-50vh",
+            transition: "0.8s",
+            borderRadius:"50%",
+            ...style,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+const Button = ({click}) => {
+  return (
+    <div className="button w-12 h-12 flex justify-center items-center gap-2 flex-col">
+      <hr className="h-1 w-8 bg-black" style={{ border: "none", transform: `${click ? "none": "rotate(45deg)"}`, transition:"0.6s"}} />
+      <hr className="h-1 w-8 bg-black" style={{ border: "none",transform: `${click ? "none": "rotate(-45deg"}`, transition:"0.6s"}} />
+    </div>
+  );
+};
+
+export default Navbar;
